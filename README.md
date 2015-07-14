31d623de62a24434097d198f744e437ee643bd41
Yves Goergen <yves@unclassified.de>
2015-07-14 15:51:50 +0200

    Added README.md and filled AssemblyInfo.cs

---
+++ README.md
@@ -0,0 +1,8 @@
+TODO Highlighter Visual Studio Extension
+========================================
+
+TodoHighlighter is an extension for Visual Studio 2010–2015 that places red boxes behind all the “TODO”s in the editor window. It can also highlight other keywords, currently “DEBUG” with a yellow colour. Collapsed code regions also show the highlighting colour so that it is easily visible where such keywords exist.
+
+Although not recommended, it is common practice to use comments with the “TODO” keyword throughout new code to indicate that something remains to be done in a certain place. And they serve as a good skeleton when writing down code for a work item, then adding the details for each part of it. These comments mostly look like other explanatory inline comments and are hard to find again unless you actually perform a text search for the keyword. This extension makes these keywords easily visible so that you quickly see where is work left to do before publishing the code.
+
+The extension does not currently have any configuration. Keywords and colours are fixed. This may change in future releases.
--- TodoHighlighter/Properties/AssemblyInfo.cs
+++ TodoHighlighter/Properties/AssemblyInfo.cs
@@ -1,33 +1,28 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TodoHighlighter")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TodoHighlighter")]
-[assembly: AssemblyCopyright("© 2015 Yves Goergen")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("TodoHighlighter")]
+[assembly: AssemblyDescription("TODO Highlighter Visual Studio Extension")]
+[assembly: AssemblyCopyright("© Yves Goergen")]
+[assembly: AssemblyCompany("unclassified software development")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Assembly identity version. Must be a dotted-numeric version.
+// NOTE: Do not edit. This value is automatically set during the build process.
+[assembly: AssemblyVersion("0.0")]
+
+// Repeat for Win32 file version resource because the assembly version is expanded to 4 parts.
+// NOTE: Do not edit. This value is automatically set during the build process.
+[assembly: AssemblyFileVersion("0.0")]
+
+// Informational version string, used for the About dialog, error reports and the setup script.
+// Can be any freely formatted string containing punctuation, letters and revision codes.
+[assembly: AssemblyInformationalVersion("0.3.0.{revnum}_{chash:6}{!:+}")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
